b1edc18bae4dfb03c716132c1454dd10a4f4b3cd
Christian Bolstad <kers@atdt.nu>
2010-10-31 19:39:19 +0100

    skeleton

---
+++ push-subscriber.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Pubsubhubbub Subscriber
+ * @author Christian Bolstad
+ * @version 1
+ */
+/*
+Plugin Name: Pubsubhubbub Subscriber
+Plugin URI: http://kracked.com/wordpress-pubsubhubbub-subscriber/
+Description: Pubsubhubbub Subscriber for Wordpress
+Author: Christian Bolstad
+Version: 1
+Author URI: http://christianbolstad.com/
+*/
+
+
+?>
