3799f80dbad592271667c7fb0c62508916b9e98b
Christian Bolstad <christian@bolstad.se>
2010-11-02 23:26:28 +0100

    parse atom from scalar

--- plugins/google_shared.php
+++ plugins/google_shared.php
@@ -1,19 +1,27 @@
 <?php
-	require 'simplepie.inc.php';
-	$url = 'http://kracked.org/wp-content/plugins/wordpress-pubsubhubbub-subscriber/testFile.txt.xml';
-	$feed = new SimplePie();
-	$feed->set_feed_url($url);
-	$feed->init();
-	// loop through items
-    foreach ($feed->get_items() as $item)
+	require_once 'simplepie.inc.php';
+
+
+	function push_parse_greader_atom($atomdata)
 	{
-		echo $item->get_permalink() . "\n";
-        $annotation = $item->get_item_tags("http://www.google.com/schemas/reader/atom/", "annotation");
-        if (isset($annotation)) 
-        {
-			$comment = $annotation[0]["child"]["http://www.w3.org/2005/Atom"]["content"][0]["data"];
-            $author = $annotation[0]["child"]["http://www.w3.org/2005/Atom"]["author"][0]["child"]["http://www.w3.org/2005/Atom"]["name"][0]["data"];
-			print " Comment: $comment Author: $author\n";    
-         }
-     }               
+		$feed = new SimplePie();
+		$feed->set_raw_data($atomdata);
+		$feed->init();
+		$feed->handle_content_type();
+		foreach ($feed->get_items() as $item)
+		{	
+			echo $item->get_permalink() . "\n";
+			$annotation = $item->get_item_tags("http://www.google.com/schemas/reader/atom/", "annotation");
+			if (isset($annotation)) 
+			{
+				$comment = $annotation[0]["child"]["http://www.w3.org/2005/Atom"]["content"][0]["data"];
+				$author = $annotation[0]["child"]["http://www.w3.org/2005/Atom"]["author"][0]["child"]["http://www.w3.org/2005/Atom"]["name"][0]["data"];
+				print " Comment: $comment Author: $author\n";    
+			 }
+		}               
+	}
+
+	$atom = file_get_contents('testFile.txt.xml');
+	push_parse_greader_atom($atom);
+
 ?>
